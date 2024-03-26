e9aef0f1768ddc9482b4d7a1fb1ae8a876ef5fc3
suprav-66 <142609701+suprav-66@users.noreply.github.com>
2024-03-26 19:49:41 +0530

    Level3

--- Task1.py
+++ Task1.py
@@ -1,13 +1,29 @@
-#String Reversal
-# Function to reverse a string
-def reverse(string):
-	string = string[::-1]
-	return string
+#Building a web scraper using python libraries 
+#pip install requests
+#pip install BeautifulSoup 
+import requests
+from bs4 import BeautifulSoup
 
-s = input("Enter a string")
+def scrape_website(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Checking if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Extract information you want from the page
+        # For example, let's say we want to extract all the links on the page
+        links = soup.find_all('a')
+        
+        # Print the extracted links
+        for link in links:
+            print(link.get('href'))
+    else:
+        print("Failed to retrieve webpage. Status :", response.status_code)
 
-print("The original string is : ", end="")
-print(s)
-#reversing using slicing operator
-print("The reversed string using slice operator is : ", end="")
-print(reverse(s))
+# Example usage
+if __name__ == "__main__":
+    url = 'https://gietu.in'  # Replace with the URL of the website you want to scrape
+    scrape_website(url)
--- Task2.py
+++ Task2.py
@@ -1,41 +1,21 @@
-# Temperature Conversion 
-# Defining a function for conversion
+#Simple Data visualization tool using python libraries
 
-def temp_conversion(temp,current_unit,to_be_convered):
-# Using "F" for Fahrenheit, "C" for Celsius and "K" for Kelvin
-# Converting from Celsius to Fahrenheit ###
-    if to_be_convered == "F" and current_unit == "C":
-        new_temp = round(9 / 5 * temp + 32, 3)
-        print("The converted temperature in Fahrenheit scale is " + str(new_temp) + " degrees")
-        
-# Converting from Fahrenheit into Celsius ###    
-    elif to_be_convered == "C" and current_unit == "F":
-        new_temp = round(5 / 9 * (temp - 32), 3)
-        print("The converted temperature in Celsius scale is " + str(new_temp) + " degrees")    
-        
-# Converting from Celsius to Kelvin ###    
-    elif to_be_convered == "K" and current_unit == "C":
-        new_temp = round(temp + 273.15, 3)
-        print("The converted temperature in Kelvin scale is " + str(new_temp) + " degrees")
-        
-# Converting from Kelvin to Celsius ###    
-    elif to_be_convered == "C" and current_unit == "K":
-         new_temp = round(temp - 273.15, 3)
-         print("The converted temperature in Celsius scale is " + str(new_temp) + " degrees")
-         
-# Converting from Kelvin to Fahrenheit ###    
-    elif to_be_convered == "F" and current_unit == "K":
-        new_temp = round(9 / 5 * (temp - 273.15) + 32, 3)
-        print("The converted temperature in Fahrenheit scale is " + str(new_temp) + " degrees")
-        
-# Converting from Fahrenheit to Kelvin ###    
-    else:
-        new_temp = round(5 / 9 * (temp - 32) + 273.15, 3)
-        print("The converted temperature in Kelvin scale is " + str(new_temp) + " degrees")     
+import pandas as pd
+import matplotlib.pyplot as plt
 
-t=float(input("Enter values in integer or float"))
-cu=input("Enter either C / K / F ")
-tc=input("Enter either C / K / F ")
 
-ob1=temp_conversion(t,cu,tc)
-      
+# reading the database
+data = pd.read_csv("Dataset.csv")
+
+# Scatter plot with day against tip
+plt.plot(data['tip'])
+plt.plot(data['size'])
+
+# Adding Title to the Plot
+plt.title("Scatter Plot")
+
+# Setting the X and Y labels
+plt.xlabel('Day')
+plt.ylabel('Tip')
+
+plt.show()
